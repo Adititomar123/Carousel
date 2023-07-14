4daccdc75d0fcb293252401bc1a3286eb989b36a
Aditi Tomar <adititomar2k8@gmail.com>
2023-07-14 13:43:21 +0530

    chores(build + style):remove open-props package - update the config file update css by removing the open-props css layers

--- postcss.config.js
+++ postcss.config.js
@@ -3,17 +3,14 @@ const postcssCustomMedia = require('postcss-custom-media')
 
 module.exports = {
   plugins: [
-    postcssCustomMedia({
-      importFrom: './node_modules/open-props/media.min.css'
-    }),
     postcssPresetEnv({
       stage: 0,
       enableClientSidePolyfills: false,
       features: {
         'cascade-layers': false,
         'clamp': false,
-        'logical-properties-and-values': false, 
-        'prefers-color-scheme-query': false, 
+        'logical-properties-and-values': false,
+        'prefers-color-scheme-query': false,
         'gap-properties': false,
         'custom-properties': false,
         'place-properties': false,
